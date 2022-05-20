8f087aec440994d9b7fb04abcfa1c229eb8d6f29
Dmytro <sunsn4ke@gmail.com>
2022-05-20 13:49:48 +0300

    refactor application to RTK Query

--- src/components/heroesAddForm/HeroesAddForm.js
+++ src/components/heroesAddForm/HeroesAddForm.js
@@ -1,20 +1,19 @@
 import { useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
 import { v4 as uiidv4 } from "uuid";
-
-import { heroAdded } from '../heroesList/heroesSlice';
 import { selectAll } from '../heroesFilters/filtersSlice';
-import { useHttp } from '../../hooks/http.hook';
+import { useCreateHeroMutation } from '../../api/apiSlice';
 import store from '../../store';
 
 const HeroesAddForm = () => {
   const [heroName, setHeroName] = useState('');
   const [heroDescr, setHeroDescr] = useState('');
   const [heroEl, setHeroEl] = useState('');
+
+  const [createHero, {isLoading, isError}] = useCreateHeroMutation();
+
   const filters = selectAll(store.getState());
   const { filtersLoadingStatus } = useSelector(state => state.filters);
-  const dispatch = useDispatch();
-  const {request} = useHttp();
 
   const onSumbitHandler = e => {
     e.preventDefault();
@@ -26,10 +25,7 @@ const HeroesAddForm = () => {
       element: heroEl
     }
 
-    request("http://localhost:3001/heroes", "POST", JSON.stringify(newHero))
-      .then(res => console.log(res, 'Success'))
-      .then(dispatch(heroAdded(newHero)))
-      .catch(err => console.log(err))
+    createHero(newHero).unwrap();
 
     setHeroName('');
     setHeroDescr('');
@@ -37,9 +33,9 @@ const HeroesAddForm = () => {
   }
 
   const elementOptions = (filters, status) => {
-    if (status === "loading") {
+    if (isLoading) {
       return <option>Загрузка элементов</option>
-    } else if (status === "error") {
+    } else if (isError) {
       return <option>Ошибка загрузки</option>
     }
 
--- src/components/heroesList/HeroesList.js
+++ src/components/heroesList/HeroesList.js
@@ -1,34 +1,39 @@
-import { useHttp } from '../../hooks/http.hook';
-import { useEffect, useCallback } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-
-import { heroDeleted, fetchHeroes, filteredHeroesSelector } from './heroesSlice';
+import { useCallback, useMemo } from 'react';
+import { useSelector } from 'react-redux';
+import { useGetHeroesQuery, useDeleteHeroMutation } from '../../api/apiSlice';
 import HeroesListItem from "../heroesListItem/HeroesListItem";
 import Spinner from '../spinner/Spinner';
 
 const HeroesList = () => {
 
-  const filteredHeroes = useSelector(filteredHeroesSelector);
-  const heroesLoadingStatus = useSelector(state => state.heroes.heroesLoadingStatus);
-  const dispatch = useDispatch();
-  const { request } = useHttp();
+  const {
+    data: heroes = [],
+    isLoading,
+    isError
+  } = useGetHeroesQuery();
 
-  useEffect(() => {
-    dispatch(fetchHeroes());
-    // eslint-disable-next-line
-  }, []);
+  const [deleteHero] = useDeleteHeroMutation();
+
+  const activeFilter = useSelector(state => state.filters.choosenFilter);
+
+  const filteredHeroes = useMemo(() => {
+    const filteredHeroes = heroes.slice();
+    if (activeFilter === 'all') {
+      return filteredHeroes;
+    } else {
+      return filteredHeroes.filter(item => item.element === activeFilter)
+    }
+  // eslint-disable-next-line
+  }, [heroes, activeFilter]);
 
   const onDelete = useCallback((id) => {
-    request(`http://localhost:3001/heroes/${id}`, 'DELETE')
-      .then(res => console.log(res, 'Deleted'))
-      .then(dispatch(heroDeleted(id)))
-      .catch(err => console.log(err))
-      // eslint-disable-next-line
-  }, [request])
+    deleteHero(id);
+    // eslint-disable-next-line
+  }, [])
 
-  if (heroesLoadingStatus === "loading") {
+  if (isLoading) {
     return <Spinner/>;
-  } else if (heroesLoadingStatus === "error") {
+  } else if (isError) {
     return <h5 className="text-center mt-5">Ошибка загрузки</h5>
   }
 
